0b53cfea82b4253827c1d1b9f47c8deb9ded7c67
lea arcidiacono <leaarcidiacono@leas-MBP.fios-router.home>
2023-12-21 23:02:47 -0500

    api fetch request

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,33 @@
-var apiUrl = 'https://type.fit/api/quotes';
+//Function to fetch quotes data
+function getQuotes() {
+  var apiUrl = "https://type.fit/api/quotes";
 
-fetch(apiUrl)
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(data) {
-    console.log(data);
-  });
+  fetch(apiUrl)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      displayQuotesData(data);
+    })
+    .catch(function (error) {
+      console.error("Error fetching quotes data:", error);
+    });
+}
+
+//Function to display quotes
+function displayQuotes(quotes) {
+  var quotesDisplay = document.getElementById("quotesDisplay");
+  quotesDisplay.innerHTML = "";
+
+  //Loop through the quotes and create elements for each quote
+  for (var i = 0; i < quotes.Data.Length; i++) {
+    var quoteElement = document.createElement("p");
+    quoteElement.textContent = quotesData[i].text;
+    // Append the quote element to the quotes display
+    quotesDisplay.appendChild(quoteElement);
+  }
+}
