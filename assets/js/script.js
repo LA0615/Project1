16c7abb4bffe6729d84d37964271fadb2f9906ce
lea arcidiacono <leaarcidiacono@leas-MBP.fios-router.home>
2023-12-23 06:59:50 -0500

    fetch quotes changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-//Function to fetch quotes data
+// Function to fetch quotes data
 function getQuotes() {
   var apiUrl = "https://type.fit/api/quotes";
 
@@ -18,16 +18,18 @@ function getQuotes() {
     });
 }
 
-//Function to display quotes
-function displayQuotes(quotes) {
+// Function to display quotes
+function displayQuotesData(quotesData) {
   var quotesDisplay = document.getElementById("quotesDisplay");
   quotesDisplay.innerHTML = "";
 
-  //Loop through the quotes and create elements for each quote
-  for (var i = 0; i < quotes.Data.Length; i++) {
+  // Loop through the quotes and create elements for each quote
+  for (var i = 0; i < quotesData.length; i++) {
     var quoteElement = document.createElement("p");
     quoteElement.textContent = quotesData[i].text;
     // Append the quote element to the quotes display
     quotesDisplay.appendChild(quoteElement);
   }
+
+  console.log("Quotes displayed:", quotesData);
 }
